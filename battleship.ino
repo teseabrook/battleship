1bc84af6fe125a30e4665f4fdeef4ac8db2082a2
teseabrook <thomasseabrook@live.co.uk>
2016-11-28 22:15:07 +0000

    Update battleship.ino

--- battleship.ino
+++ battleship.ino
@@ -3,7 +3,6 @@
 //OLED Display and Rotary Encoder setup code taken from classwork
 #include <Interrupt.h>
 #include <TimerListener.h>
-#include <RifIntEncoder.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -18,7 +17,6 @@
 //Global Variables/Classes
 Adafruit_SSD1306 display(9, 10, 12);
 TM1638 module(7, 6, 5);
-RifIntEncoder knob;
 int8_t player1ShipsRemaining = 5;
 int8_t player2ShipsRemaining = 5;
 bool gameComplete = false;
@@ -30,10 +28,10 @@ int8_t shipPos[2][5][2][5] = {{{{0,0,0,0,0}, {0,0,0,0,0}},
   {{0,0,NULL, NULL, NULL}, {0,0,NULL, NULL, NULL}}},
   
   {{{0,0,0,0,0}, {0,0,0,0,0}},
-  {{0,0,0,0,0}, {0,0,0,0,0}},
-  {{0,0,0,0,0}, {0,0,0,0,0}},
-  {{0,0,0,0,0}, {0,0,0,0,0}},
-  {{0,0,0,0,0}, {0,0,0,0,0}}}};
+  {{0,0,0,0,NULL}, {0,0,0,0,NULL}},
+  {{0,0,0,NULL, NULL}, {0,0,0,NULL, NULL}},
+  {{0,0,0,NULL, NULL}, {0,0,0,NULL, NULL}},
+  {{0,0,NULL, NULL, NULL}, {0,0,NULL, NULL, NULL}}}};
 //First is player, second is ship, third is x or y coords and 4th is the coords themselves
 
 int8_t gameBoards[2][10][10] = {{{0,0,0,0,0,0,0,0,0,0},
@@ -84,6 +82,11 @@ void placeShip(int8_t, int8_t);
 int8_t displayToSevenSeg(int8_t);
 void displayToSevenSeg(int8_t, int8_t);
 void getPos(int8_t *p1, int8_t *p2);
+void loadInToArray(int8_t[2], int8_t[2], int8_t, int8_t, int8_t);
+void fire(int8_t);
+void getHit(int8_t[2], int8_t);
+void complete();
+void hasWon(int8_t);
 // ------------------------------------------------------------------------------------
 void setup()
 {
@@ -99,10 +102,6 @@ void setup()
   display.setTextSize(1);
   display.setTextColor(WHITE);
   Interrupt::initialiseStaticISRVectorTable() ;
-  // -- encoders ------------------------------------------------
-  knob.init(3, 4);
-  knob.setMax(180);
-  knob.setMin(0);
 }
 
 void loop()
@@ -113,52 +112,54 @@ void loop()
 void menu()
 {
   int choice = 0;
-
-  while (go)
+  buttons = 0;
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("Battleship Game v1.0");
+  display.println("[Play]");
+  display.println(" Controls ");
+  display.println(" Exit ");
+  display.display();
+  while (true)
   {
-    delay(500);
-    int knobPos = knob.getPos();
-
-      if (knobPos % 3 == 0)
-      {
-        choice = 0;
-        display.clearDisplay();
-        display.setCursor(0, 0);
-        display.println("Battleship Game v1.0");
-        display.println("[Play]");
-        display.println(" Controls ");
-        display.println(" Exit ");
-        display.display();
-
-      }
-      else if (knobPos % 2 == 0)
-      {
-        choice = 1;
-        display.clearDisplay();
-        display.setCursor(0, 0);
-        display.println("Battleship Game v1.0");
-        display.println(" Play ");
-        display.println("[Controls]");
-        display.println(" Exit ");
-        display.display();
-
-      }
-      else
-      {
-        choice = 2;
-        display.clearDisplay();
-        display.setCursor(0, 0);
-        display.println("Battleship Game v1.0");
-        display.println(" Play ");
-        display.println(" Controls ");
-        display.println("[Exit]");
-        display.display();
-
-      }
-    
-    buttons = module.getButtons();
-
-    if (buttons == 128 && choice == 0)
+    do
+    {
+      buttons = module.getButtons();
+    } while (buttons == 0);
+    if (buttons == 1)
+    {
+      choice = 0;
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.println("Battleship Game v1.0");
+      display.println("[Play]");
+      display.println(" Controls ");
+      display.println(" Exit ");
+      display.display();
+    }
+    else if (buttons == 2)
+    {
+      choice = 1;
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.println("Battleship Game v1.0");
+      display.println(" Play ");
+      display.println("[Controls]");
+      display.println(" Exit ");
+      display.display();
+    }
+    else if (buttons == 4)
+    {
+      choice = 2;
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.println("Battleship Game v1.0");
+      display.println(" Play ");
+      display.println(" Controls ");
+      display.println("[Exit]");
+      display.display();
+    }
+    else if (buttons == 128 && choice == 0)
     {
       play();
     }
@@ -184,9 +185,13 @@ void play()
     display.println("Press S8 to begin placing your Aircraft Carrier.");
     display.display();
     delay(1000);
-    while(true)
+    bool placed = false;
+    while(!placed)
     {
-      buttons = module.getButtons();
+      do
+      {
+        buttons = module.getButtons();
+      } while (buttons == 0);
       if(buttons == 128)
       {
         play = false;
@@ -194,9 +199,163 @@ void play()
         drawBoard(0,0);
         delay(1000);
         placeShip(0, 0);
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.println("       Player 1");
+        display.println("Place your cruiser");
+        delay(1000);
+        drawBoard(0,0);
+        placeShip(0, 1);
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.println("       Player 1");
+        display.println("Place your Submarine");
+        delay(1000);
+        drawBoard(0,0);
+        placeShip(0, 2);
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.println("       Player 1");
+        display.println("Place your Frigate");
+        delay(1000);
+        drawBoard(0,0);
+        placeShip(0, 3);
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.println("       Player 1");
+        display.println("Place your Destroyer");
+        delay(1000);
+        drawBoard(0,0);
+        placeShip(0, 4);
+      }
+    }
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("       Player 2");
+    display.println("Press S8 to begin placing your Aircraft Carrier.");
+    display.display();
+    delay(1000);
+    do
+      {
+        buttons = module.getButtons();
+      } while (buttons == 0);
+      if(buttons == 128)
+      {
+        play = false;
+        display.clearDisplay();
+        drawBoard(1,1);
+        delay(1000);
+        placeShip(1, 0);
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.println("       Player 1");
+        display.println("Place your cruiser");
+        delay(1000);
+        drawBoard(1,1);
+        placeShip(1, 1);
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.println("       Player 1");
+        display.println("Place your Submarine");
+        delay(1000);
+        drawBoard(1,1);
+        placeShip(1, 2);
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.println("       Player 1");
+        display.println("Place your Frigate");
+        delay(1000);
+        drawBoard(1,1);
+        placeShip(1, 3);
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.println("       Player 1");
+        display.println("Place your Destroyer");
+        delay(1000);
+        drawBoard(1,1);
+        placeShip(1, 4);
+        placed = true;
+      }
+  }
+}
+
+void fire(int8_t player)
+{
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("Player ");
+  display.print(player);
+  display.println("'s turn");
+  switch(player)
+  {
+    case 0:
+      drawBoard(1, 0);
+      break;
+    case 1:
+      drawBoard(0, 1);
+      break;
+  }
+  do
+  {
+    int8_t desiredPos[2] = {0, 0};
+    getPos(&desiredPos[0], &desiredPos[1]);
+  } while (desiredPos[0] > 127 || desiredPos[1] > 127);
+  gameBoards[desiredPos[0][desiredPos[1]]] += 128;
+  getHit(desiredPos, player);
+}
+
+void getHit(int8_t pos[2], player)
+{
+  if(player == 1)
+  {
+    player = 0;
+  }
+  else
+  {
+    player = 1;
+  }
+  for(int j = 0; j < 5; j++)
+  {
+    for(int i = 0; i < 5; i++)
+    {
+       if(pos[0] == shipPos[player][j][0][i] && pos[1] == shipPos[player][j][1][i])
+       {
+        shipPos[player][j][0][i] = NULL;
+        shipPos[player][j][1][i] = NULL;
+        hasWon();
       }
     }
   }
+}
+
+void hasWon()
+{
+  for(int i = 0; i < 2; i++)
+  {
+    playerRemaining = 5;
+    for(int j = 0; j < 5; j++)
+    {
+      if(shipPos[i][j][0][0] == NULL && shipPos[i][j][0][1] == NULL && shipPos[i][j][0][2] == NULL && shipPos[i][j][0][3] == NULL && shipPos[i][j][0][4] == NULL)
+      {
+        playerRemaining--;
+      }
+    }
+    if(playerRemaining == 0)
+    {
+      complete(i)
+    }
+  }
+}
+
+void complete(int8_t player)
+{
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print("Player ");
+  display.print(player + 1);
+  display.println(" wins!");
+  gameComplete = true;
+  delay(5000);
 }
 
 void drawBoard(int8_t board, int8_t player)
@@ -232,6 +391,20 @@ void drawBoard(int8_t board, int8_t player)
         }
       }
     }
+  }
+  int turns = 0;
+  while(!gameComplete)
+  {
+    if(turns % 2 = 1)
+    {
+      fire(1);
+    }
+    else
+    {
+      fire(0);
+    }
+    turns++;
+    //Stops things like being able to fire after the game ends
   }
 }
 
@@ -278,33 +451,101 @@ void placeShip(int8_t player, int8_t ship)
   do
   {
     cont = true;
+    Serial.println("Getting Input");
     getPos(p1, p2);
+    Serial.print("Debug ");
+    Serial.println(startPos[0]);
     getPos(p3, p4);
-    if(((startPos[0] != endPos[0]) || (startPos[1] != endPos[1])) || ((startPos[0] - endPos[0] != len) || (startPos[1] - endPos[1] != len) || (endPos[0] - startPos[0] != len) || (endPos[1] - startPos[1] != len)))
+    Serial.print("Debug2 ");
+    Serial.println(endPos[0]);
+    if(((startPos[0] == endPos[0]) || (startPos[1] == endPos[1])) && ((startPos[0] - endPos[0] == (len-1)) || (startPos[1] - endPos[1] == (len-1)) || (endPos[0] - startPos[0] == (len-1)) || (endPos[1] - startPos[1] == (len-1))))
     {
+      Serial.println("Valid");
       cont = false;
     }
     if(cont)
     {
       display.clearDisplay();
       display.setCursor(0,0);
-      display.println("Invalid input");
+      Serial.println("Invalid input");
       delay(1000);
+      display.clearDisplay();
+      display.display();
       drawBoard(player, player);
     }
   } while (cont);
+
+  loadInToArray(startPos, endPos, player, ship, len);
   
+}
+
+void loadInToArray(int8_t startPos[2], int8_t endPos[2], int8_t player, int8_t ship, int8_t len)
+{
+  Serial.println("Loading");
+  if(startPos[0] == endPos[0])
+  {
+    if(startPos[1] > endPos[1])
+    {
+      for(int8_t i = 0; i < len; i++)
+      {
+        Serial.println("Loading.");
+        shipPos[player][ship][0][i] = startPos[0];
+        shipPos[player][ship][1][i] = endPos[1] + i;
+      }
+    }
+    else
+    {
+      for(int8_t i = 0; i > len; i++)
+      {
+        Serial.println("Loading.");
+        shipPos[player][ship][0][i] = startPos[0];
+        shipPos[player][ship][1][i] = startPos[1] + i;
+      }
+    }
+  }
+  else
+  {
+    if(startPos[0] > endPos[0])
+    {
+      for(int8_t i = 0; i < len; i++)
+      {
+        Serial.println("Loading.");
+        shipPos[player][ship][1][i] = startPos[1];
+        shipPos[player][ship][0][i] = endPos[0] + i;
+      }
+    }
+    else
+    {
+      for(int8_t i = 0; i > len; i++)
+      {
+        Serial.println("Loading.");
+        shipPos[player][ship][1][i] = startPos[1];
+        shipPos[player][ship][0][i] = startPos[0] + i;
+      }
+    }
+  }
+  Serial.println(shipPos[player][ship][0][0]);
+  for(int8_t i = 0; i > len; i++)
+  {
+    gameBoards[player][shipPos[player][ship][0][i]] += 1;
+    gameBoards[player][shipPos[player][ship][1][i]] += 1;
+  }
 }
 
 void getPos(int8_t *p1, int8_t *p2)
 {
-    int8_t desiredPos[2] = {0,0};
+  int8_t desiredPos[2] = {0,0};
   int8_t buttonPos[5] = {1, 2, 3, 4, 5};
   bool confirm = false;
-  while(desiredPos[1] == 0 && !confirm)
+  while(desiredPos[1] == 0 && confirm == false)
   {
     confirm = false;
-    buttons = module.getButtons();
+    delay(500);
+    buttons = 0;
+    do
+    {
+      buttons = module.getButtons();
+    } while (buttons == 0);
     switch(buttons)
     {
       case 1:
@@ -413,10 +654,13 @@ void getPos(int8_t *p1, int8_t *p2)
       default:
         break;
     }
-    Serial.println(confirm);
   }
+  
+    Serial.print(desiredPos[0]);
+    Serial.println(desiredPos[1]);
+    Serial.println("Exit");
   int8_t temp[8] = {0,0,0,0,0,0,0,0};
- //   module.setDisplay(temp);
+   module.setDisplay(temp);
     *p1 = desiredPos[0];
     *p2 = desiredPos[1];
 }
@@ -428,7 +672,11 @@ void controls()
   while (stay)
   {
     printOne();
-    buttons = module.getButtons();
+    buttons = 0;
+    do
+    {
+      buttons = module.getButtons();
+    } while (buttons == 0);
     if (buttons == 64)
     {
       controlsPageTwo();
@@ -449,52 +697,42 @@ int8_t displayToSevenSeg(int8_t num)
     case 1: //A
       sevenSegData[0] = 0b01110111;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("a");
       break;
     case 2://b
       sevenSegData[0] = 0b01111100;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("b");
       break;
     case 3://C
       sevenSegData[0] = 0b00111001;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("c");
       break;
     case 4://d
       sevenSegData[0] = 0b01011110;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("d");
       break;
    case 5://E
       sevenSegData[0] = 0b01111001;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("e");
       break;
    case 6://F
       sevenSegData[0] = 0b01110001;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("f");
       break;
    case 7://g
       sevenSegData[0] = 0b01101111;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("g");
       break;
    case 8://H
       sevenSegData[0] = 0b01110110;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("h");
       break;
    case 9://I
       sevenSegData[0] = 0b00000110;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("i");
       break;
    case 10://J
       sevenSegData[0] = 0b00001110;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("j");
       break;
    default:
       break;
@@ -511,52 +749,42 @@ void displayToSevenSeg(int8_t num1, int8_t num2)
     case 1: //1
       sevenSegData[1] = 0b00110000;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("1");
       break;
     case 2://2
       sevenSegData[1] = 0b01011011;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("2");
       break;
     case 3://3
       sevenSegData[1] = 0b01001111;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("3");
       break;
     case 4://4
       sevenSegData[4] = 0b01100110;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("4");
       break;
    case 5://5
       sevenSegData[0] = 0b01101101;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("5");
       break;
    case 6://6
       sevenSegData[0] = 0b01111101;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("6");
       break;
    case 7://7
       sevenSegData[0] = 0b00000111;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("7");
       break;
    case 8://8
       sevenSegData[0] = 0b01111111;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("8");
       break;
    case 9://9
       sevenSegData[0] = 0b01101111;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("9");
       break;
    case 10://0
       sevenSegData[0] = 0b00111111;
       module.setDisplay(sevenSegData, 8);
-      Serial.println("10");
       break;
    default:
       break;
@@ -587,7 +815,11 @@ void controlsPageTwo()
   while (true)
   {
     printTwo();
-    buttons = module.getButtons();
+    buttons = 0;
+    do
+    {
+      buttons = module.getButtons();
+    } while (buttons == 0);
     if (buttons == 64)
     {
       controlsPageThree();
@@ -645,7 +877,11 @@ void controlsPageFour()
   while (true)
   {
     printFour();
-    buttons = module.getButtons();
+    buttons = 0;
+    do
+    {
+      buttons = module.getButtons();
+    } while (buttons == 0);
     if (buttons == 32)
     {
       delay(500);
